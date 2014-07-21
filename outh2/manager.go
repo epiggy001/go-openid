73ec39cfacd13ae11ab28d7624ea5bcba2686003
jackeychen <jackeychen@clustertech.com>
2014-07-21 11:49:27 +0800

    Begin wiht auth request

--- outh2/manager.go
+++ outh2/manager.go
@@ -1,5 +1,13 @@
 package oauth2
 
+import (
+  "net/http"
+)
+
+const (
+  authorizationCode = "authorization_code"
+)
+
 type ClientManager interface {
   Insert(c *Client) (string, error)
   Remove(id string) error
@@ -22,4 +30,63 @@ type RefreshTokenManager interface {
   Insert(t *RefreshToken) (string, error)
   Remove(id string) error
   Read(id string) (*RefreshToken, error)
+}
+
+type managers struct {
+  Client       ClientManager
+  Code         CodeManager
+  Token        TokenManager
+  RefreshToken RefreshTokenManager
+}
+
+type OauthManager struct {
+  CodeLife         int32
+  TokenLife        int32
+  RefreshTokenLife int32
+  AllowGetMethod   bool
+
+  Managers *managers
+
+  ClientAuthFunc func(r *http.Request, c *Client) bool
+}
+
+func (m *OauthManager) getClient(r *http.Request) (*Client, error) {
+  clientId := r.Form.Get("client_id")
+  client, err := m.Managers.Client.Read(clientId)
+  if err != nil {
+    return nil, NewAuthError(nil, E_INVALID_CLIENT, err.Error())
+  }
+
+  if client == nil {
+    return nil, NewAuthError(nil, E_INVALID_CLIENT, "Failt to read client")
+  }
+
+  if !m.ClientAuthFunc(r, client) {
+    return nil, NewAuthError(nil, E_INVALID_CLIENT, "Failt to validate client")
+  }
+
+  if (r.Method == "GET" && !m.AllowGetMethod) || (r.Method != "POST") {
+    return nil, NewAuthError(nil, E_INVALID_REQUEST, "Invalid request method")
+  }
+  return client, nil
+}
+
+func (m *OauthManager) HandleAuthRequest(w http.ResponseWriter,
+  r *http.Request) error {
+  err := r.ParseForm()
+  if err != nil {
+    return NewAuthError(nil, E_INVALID_REQUEST, err.Error())
+  }
+
+  _, err = m.getClient(r)
+  if err != nil {
+    return err
+  }
+
+  grantType := r.Form.Get("grant_type")
+  if grantType != authorizationCode {
+    return NewAuthError(nil, E_INVALID_REQUEST, "Fail to parse request")
+  }
+
+  return nil
 }
