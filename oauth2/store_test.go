bb0a546b3de2ef1b84bbd07c3f33994e7ddf3717
jackeychen <jackeychen@clustertech.com>
2014-07-25 15:53:31 +0800

    Add test for token store

--- oauth2/store_test.go
+++ oauth2/store_test.go
@@ -43,4 +43,37 @@ func TestMemoryStoreRemove(t *testing.T) {
 }
 
 func TestNewTokenStore(t *testing.T) {
+  m := NewTokenStore()
+  if m == nil || m.store == nil {
+    t.Error("Fail to create token store")
+  }
+}
+
+func TestTokenStoreSaveAndRead(t *testing.T) {
+  m := NewTokenStore()
+  token := NewToken(kClientId, kScope, kBaseUri, kLife)
+  id, err := m.Save(token)
+  if err != nil {
+    t.Error("Fail to save token")
+  }
+  rtoken, err := m.Read(id)
+  if err != nil {
+    t.Error("Fail to read token")
+  }
+  if rtoken != token {
+    t.Error("Read wrong token")
+  }
+}
+
+func TestTokenStoreRemove(t *testing.T) {
+  m := NewTokenStore()
+  token := NewToken(kClientId, kScope, kBaseUri, kLife)
+  id, _ := m.Save(token)
+  err := m.Remove(id)
+  if err != nil {
+    t.Error("Fail to remove token")
+  }
+  if rtoken, _ := m.Read(id); rtoken != nil {
+    t.Error("Fail to remove token but no error reported")
+  }
 }
