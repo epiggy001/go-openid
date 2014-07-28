a27197f7beb8561d50f96351232f0a92c3e54425
jackeychen <jackeychen@clustertech.com>
2014-07-28 17:37:49 +0800

    Fix bug for example

--- oauth2/client.go
+++ oauth2/client.go
@@ -18,3 +18,27 @@ func (c *Client) String() string {
 func NewClient(id, secret, uri, des string) *Client {
   return &Client{id, secret, uri, des}
 }
+
+type MockClientStore struct {
+  store *MemoryStore
+}
+
+func NewMockClientStore() *MockClientStore {
+  return &MockClientStore{NewMemoryStore()}
+}
+
+func (m *MockClientStore) Save(c *Client) (string, error) {
+  m.store.Save(c.Id, c)
+  return c.Id, nil
+}
+
+func (m *MockClientStore) Remove(id string) error {
+  m.store.Remove(id)
+  return nil
+}
+
+func (m *MockClientStore) Read(id string) (*Client, error) {
+  c, _ := m.store.Read(id).(*Client)
+  return c, nil
+}
+
